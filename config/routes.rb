edfc1ad54a2dd7c82c2359ba80ed351d06daa565
Lucas Malta <lucas50xd@hotmail.com>
2018-04-06 01:20:08 -0300

    adding login page
    Co-authored-by: affbedran <bedran.fleck@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  root 'welcome#index'
+  
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
