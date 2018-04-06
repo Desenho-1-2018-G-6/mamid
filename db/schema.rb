ba158ae55e81058b721cf4fff83953f9e42e964f
ThalissonMelo <tallisonmelo@hotmail.com>
2018-04-06 03:23:14 -0300

    Adding datase and view changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405140115) do
+ActiveRecord::Schema.define(version: 20180406052755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,10 @@ ActiveRecord::Schema.define(version: 20180405140115) do
     t.string   "cpf"
     t.date     "birth_date"
     t.string   "gender"
-    t.integer  "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "phone"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
