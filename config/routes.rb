1fedae8bb4585e962d394435011fcf584fdff8a5
ThalissonMelo <tallisonmelo@hotmail.com>
2018-04-06 03:23:14 -0300

    Adding datase and view changes
    Co-authored-by: Ícaro Oliveira <icarooliv2@gmail.com>
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :users
+  root 'sessions#new'
+  get 'sessions/new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  get '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20180405140115_create_users.rb
+++ db/migrate/20180405140115_create_users.rb
@@ -1,12 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
       t.string :email
       t.string :cpf
       t.date :birth_date
       t.string :gender
-      t.integer :phone
+      t.string :phone
 
       t.timestamps
     end
