85ca631d205d523040c5c631a0faf2c1fd294ac5
Guilherme Augusto <guilherme.francais@gmail.com>
2018-04-13 16:16:18 -0300

    Builds routes and migrations to Admin class
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>
    Co-authored-by: Ateldy Filho <ateldybfilho@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180412140043) do
+ActiveRecord::Schema.define(version: 20180413175355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +40,7 @@ ActiveRecord::Schema.define(version: 20180412140043) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.boolean  "admin"
   end
 
 end
