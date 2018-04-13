2674b0d9cad8d2a68440cb76bf74ca0ad64bf7be
Guilherme Lacerda <guiglacerda@gmail.com>
2018-04-13 11:12:16 -0300

    Improves product page

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
   validates :name, presence: true, length: { in: 2..100 }
   validates :description, presence: true
   validates :price, presence: true
+  validates :stock, presence: true, :numericality => {:greater_than_or_equal_to => 0}
 
 end
