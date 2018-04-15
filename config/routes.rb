fe82769b27671a4bbe189c9dfaaea188856f3733
ThalissonMelo <tallisonmelo@hotmail.com>
2018-04-15 18:25:49 -0300

    Adding routes to cart
    Co-authored-by: ThalissonMelo <tallisonmelo@hotmail.com>
    Co-authored-by: icarooliv <icarooliv2@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,13 @@ Rails.application.routes.draw do
   resources :products
   resources :users
 
-  root 'sessions#new'
   get 'sessions/new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   get '/logout',  to: 'sessions#destroy'
   
+  get 'cart/:id',         to: 'carts#show'
+  get 'cart/:id/update',  to: 'carts#update'
+  get 'cart/:id/destroy', to: 'carts#destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
