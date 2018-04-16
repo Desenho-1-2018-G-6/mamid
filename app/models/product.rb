c39d31444241df3c70f29e7e5ad396d33a351671
Guilherme Lacerda <guiglacerda@gmail.com>
2018-04-16 11:08:52 -0300

    Improves product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
-  validates :name, presence: true, length: { in: 2..100 }
+  validates :name, presence: true, length: { in: 2..150 }
   validates :description, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, :numericality => {:greater_than_or_equal_to => 0}
   validates :stock, presence: true, :numericality => {:greater_than_or_equal_to => 0}
 
 end
