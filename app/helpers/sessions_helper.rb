b363788d0d4a627150e5a395530e813ea19c3672
Lucas Malta <lucas50xd@hotmail.com>
2018-04-16 10:58:54 -0300

    sets up require_login method and some redirects

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -50,3 +50,10 @@ module SessionsHelper
   end
 
 end
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to login_url # halts request cycle
+    end
+  end
+end
