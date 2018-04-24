7e2be165c660da866e4ed482a18901a904865d2f
ThalissonMelo <tallisonmelo@hotmail.com>
2018-04-23 22:30:56 -0300

    Adding current order method to application controller
    Co-authored-by: ThalissonMelo <tallisonmelo@hotmail.com>
    Co-authored-by: icarooliv <icarooliv2@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+  helper_method :current_order
+
+  def current_order
+    if !session[:order_id].nil?
+      Order.find(session[:order_id])
+    else
+      @order_status = OrderStatus.create(name: "In Progress")
+      Order.new(order_status_id: @order_status.id)
+    end
+  end
+
+
   helper CSSModules::ViewHelper
-  
+
   include SessionsHelper
 end
