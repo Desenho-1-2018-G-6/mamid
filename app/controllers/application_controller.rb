6f57f1489f837936c2d6e02c8cda3b4d3bd163c7
Ícaro <icarooliv2@gmail.com>
2018-04-24 00:30:30 -0300

    refactoring order itens controller and products view
        Co-authored-by: ThalissonMelo <tallisonmelo@hotmail.com>
        Co-authored-by: icarooliv <icarooliv2@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_order
+  add_flash_types :success, :warning, :danger, :info
 
   def current_order
     if !session[:order_id].nil?
--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,27 +2,38 @@ class OrderItemsController < ApplicationController
   def create
     @order = current_order
     @order_item = @order.order_items.new(order_item_params)
-    @order.save
-    session[:order_id] = @order.id
-
+    if @order.save  
+      session[:order_id] = @order.id
+      # format.json { render json: @order, status: :created }
+    else
+      format.json { render json: @order.errors.full_messages, status: :unprocessable_enity }
+    end
     redirect_back(fallback_location: 'back')
   end
 
   def update
     @order = current_order
     @order_item = @order.order_items.find(params[:id])
-    @order_item.update_attributes(order_item_params)
-    @order_items = @order.order_items
+    if @order_item.update_attributes(order_item_params)
+       @order_items = @order.order_items
+    else
+      format.json { render json: @order_item.errors.full_messages, status: :unprocessable_enity }
+    end
   end
 
   def destroy
     @order = current_order
     @order_item = @order.order_items.find(params[:id])
-    @order_item.destroy
-    @order_items = @order.order_items
+    if @order_item.destroy
+       @order_items = @order.order_items
+    else 
+       format.json { render json: @order_item.errors.full_messages, status: :unprocessable_enity }
+    end
   end
-private
+
+  private
   def order_item_params
     params.permit(:quantity, :product_id)
   end
+
 end
