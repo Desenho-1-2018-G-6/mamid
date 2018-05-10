893b260a8ffb863b08cba7e90e3ed2ff25056561
ThalissonMelo <tallisonmelo@hotmail.com>
2018-05-10 15:44:24 -0300

    Change way to call state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,27 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_order
   add_flash_types :success, :warning, :danger, :info
+  before_action :verify_state
 
   def current_order
-    if !session[:order_id].nil?
+    unless session[:order_id].nil?
       Order.find(session[:order_id])
     else
-      @in_progress = InProgressState.new
-      @order_status = OrderStatus.create(state: @in_progress.status)
-      Order.new(order_status_id: @order_status.id)
+      Order.new
     end
   end
 
+  def verify_state
+    unless session[:order_id].nil?
+      if current_order.order_items.empty?
+        @state = EmptyState.new.status
+      else
+        @status = InProgressState.new.status
+      end
+    else
+      @state = EmptyState.new.status
+    end
+  end
 
   helper CSSModules::ViewHelper
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,4 @@
 class Order < ActiveRecord::Base
-  belongs_to :order_status
   has_many :order_items
   before_create :set_order_status
   before_save :update_subtotal
